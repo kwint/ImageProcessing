555373fc44c1799f5d1a9d94d913dc397d63c5e4
kwint <quintvdijk@gmail.com>
2018-09-05 13:17:43 +0200

    Added contrast en brightness with sliders

--- main.py
+++ main.py
@@ -5,11 +5,29 @@ Tested on Basler acA1300-200uc (USB3, linux 64bit , python 3.5)
 from pypylon import pylon
 import cv2
 
+
+
+def nothing(x):
+    pass
+
+def change_brightness_contrast(img, b, c):
+    return cv2.addWeighted(img, 1. + c / 127., img, 0, b - c)
+
+
+cv2.namedWindow('slider', cv2.WINDOW_AUTOSIZE)
+cv2.moveWindow('slider', 640, 0)
+cv2.resizeWindow('slider', 560, 400)
+cv2.createTrackbar('B', 'slider', 0, 255, nothing)
+cv2.createTrackbar('C', 'slider', 0, 255, nothing)
+
+
 # conecting to the first available camera
 camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 
 # Grabing Continusely (video) with minimal delay
 camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+camera.GetDeviceInfo().SetPropertyValue("Expose time", "8")
+
 converter = pylon.ImageFormatConverter()
 
 # converting to opencv bgr format
@@ -17,14 +35,16 @@ converter.OutputPixelFormat = pylon.PixelType_BGR8packed
 converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
 while camera.IsGrabbing():
-    grabResult = camera.RetrieveResult(20, pylon.TimeoutHandling_ThrowException)
+    grabResult = camera.RetrieveResult(20, pylon.TimeoutHandling_ThrowException)  # Change number for speed
 
     if grabResult.GrabSucceeded():
         # Access the image data
         image = converter.Convert(grabResult)
         img = image.GetArray()
-        cv2.namedWindow('title', cv2.WINDOW_NORMAL)
-        cv2.imshow('title', img)
+        cv2.namedWindow('normal', cv2.WINDOW_NORMAL)
+        cv2.namedWindow('edited', cv2.WINDOW_NORMAL)
+        cv2.imshow('normal', img)
+        cv2.imshow('edited', change_brightness_contrast(img, cv2.getTrackbarPos('B', 'slider'), cv2.getTrackbarPos('C', 'slider')))
         k = cv2.waitKey(1)
         if k == 27:
             break
